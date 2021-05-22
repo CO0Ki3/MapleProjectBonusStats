b5be7c69fd4aae3f004887bdb02ea0e2d3aac105
co0ki3 <ehrrhgus0324@gmail.com>
2021-05-22 18:02:30 +0900

    Use Context as store

---
+++ src/hook.js
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+const StoreContext = React.createContext(null);
+
+const initialStore = {
+  list: '',
+  sort: '',
+  box: [],
+}
+
+export const StoreProvider = props => {
+  const [ store, setStore ] = useState(initialStore);
+
+  return (
+    <StoreContext.Provider value={[ store, setStore ]}>
+      {props.children}
+    </StoreContext.Provider>
+  )
+}
+
+export const useStore = (key=undefined) => {
+  const [ store, setStore ] = useContext(StoreContext);
+  const [ value, setValue ] = useState();
+
+  useEffect(() => {
+    if (typeof key !== undefined) {
+      setValue(store[key]);
+    } else {
+      setValue(store);
+    }
+  }, [key, store])
+
+  const setFoo = v => {
+    if (typeof key !== undefined) {
+      setStore({ ...store, [key]: v })
+    } else {
+      setStore(v);
+    }
+  }
+  
+  console.log(store);
+
+  return [ value, setFoo ];
+}
