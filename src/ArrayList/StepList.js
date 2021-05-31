1f691ce5426e968b2dca5550c5b16029f925bf2d
co0ki3 <ehrrhgus0324@gmail.com>
2021-05-31 20:26:40 +0900

    Change Keys

--- src/ArrayList/StepList.js
+++ src/ArrayList/StepList.js
@@ -1,24 +1,24 @@
 const Step = [
-    {
-        value: "7",
-        text: "1추"
-    },
-    {
-        value: "6",
-        text: "2추"
-    },
-    {
-        value: "5",
-        text: "3추"
-    },
-    {
-        value: "4",
-        text: "4추"
-    },
-    {
-        value: "3",
-        text: "5추"
-    }
+  {
+    value: "7",
+    text: "1추",
+  },
+  {
+    value: "6",
+    text: "2추",
+  },
+  {
+    value: "5",
+    text: "3추",
+  },
+  {
+    value: "4",
+    text: "4추",
+  },
+  {
+    value: "3",
+    text: "5추",
+  },
 ];
 
 export default Step;
